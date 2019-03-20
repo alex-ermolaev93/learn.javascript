68f63df0165deda0933e8572973a66a264b65909
Aleksandr Yermolaev <ermolaev.jp@gmail.com>
2019-03-20 10:42:26 +0200

    function works!

--- JavaScript/17_function.js
+++ JavaScript/17_function.js
@@ -1,7 +1,20 @@
+checkAge();
 function checkAge(age) {
+    return (age>18) ||  confirm('Parents allowed?');
+} 
+
+
+/*function checkAge(age) {
+    return (age>18) ? true : confirm('Parents allowed?');
+}
+*/
+
+
+/*function checkAge(age) {
     if (age>18) {
              return true;
     }else{
              return confirm('Parents allowed?');
      }
 }
+*/
