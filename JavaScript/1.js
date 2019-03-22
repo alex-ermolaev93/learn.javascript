d908bae4eb9898d34b2befa70dfbe5b2d2659f40
Aleksandr Yermolaev <ermolaev.jp@gmail.com>
2019-03-22 15:47:55 +0200

    while,switch

---
+++ JavaScript/1.js
@@ -0,0 +1,22 @@
+// var i = 10;
+// while(i) {
+//     console.log(i);
+// }
+// var i;
+// for (i = 0; i <= 10; i = i + 2) {
+//     console.log(i);
+// }
+var i = +prompt('number?','hfcghd');
+switch (i) {
+    case 5:
+        console.log('ok');
+        break;
+    case 9:
+        console.log('okey');
+        break;
+    case 15:
+        console.log('not ok');
+        break;
+    default:
+        console.log('no no no!!!')
+}
